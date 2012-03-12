4e8d2b7d12da2aa3c91aa4790504a00fd9d68dc4
IsmAvatar <IsmAvatar@gmail.com>
2012-03-11 21:46:08 -0400

    Added icons, front-end (mockui), a few more components. Hooked in front-end to main frame

--- org/lateralgm/libmaker/LibMaker.java
+++ org/lateralgm/libmaker/LibMaker.java
@@ -2,53 +2,230 @@
  * Copyright (C) 2012 IsmAvatar <IsmAvatar@gmail.com>
  * 
  * This file is part of LibMaker.
- * LibMaker is free software and comes with ABSOLUTELY NO WARRANTY.
- * See LICENSE for details.
+ * 
+ * LibMaker is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * LibMaker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License (COPYING) for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.lateralgm.libmaker;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 import java.io.File;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JToolBar;
 import javax.swing.UIManager;
 
 import org.lateralgm.libmaker.backend.Library;
+import org.lateralgm.libmaker.components.GmMenu;
 import org.lateralgm.libmaker.file.FileChooser;
+import org.lateralgm.libmaker.mockui.MockUI;
 
-public class LibMaker extends JFrame
+public class LibMaker extends JFrame implements ActionListener
 	{
 	private static final long serialVersionUID = 1L;
 
 	protected FileChooser fc;
+	protected MockUI ui;
 	protected Library currentLib;
 	protected boolean modified;
 
 	public LibMaker()
 		{
 		super(); //Title gets set when library is set
+		ui = new MockUI();
 		fc = new FileChooser(this);
 
+		setJMenuBar(createMenu());
+		add(createTool(),BorderLayout.NORTH);
+
+		add(ui,BorderLayout.CENTER);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		pack();
-		setSize(350,350); //XXX: Delete once components are laid out.
 		setLocationRelativeTo(null);
 		setMinimumSize(getSize());
 
 		setLibrary(new Library()); //Now that we have our needed size, setup with our initial library
 		}
 
+	protected JMenuBar createMenu()
+		{
+		JMenuBar mb = new JMenuBar();
+		GmMenu menu;
+
+		mb.add(menu = new GmMenu(Messages.getString("Menu.FILE"),this)); //$NON-NLS-1$
+		menu.addItem("Menu.NEW",KeyEvent.VK_N,InputEvent.CTRL_DOWN_MASK); //$NON-NLS-1$
+		menu.addItem("Menu.OPEN",KeyEvent.VK_O,InputEvent.CTRL_DOWN_MASK); //$NON-NLS-1$
+		menu.addSeparator();
+		menu.addItem("Menu.SAVE",KeyEvent.VK_S,InputEvent.CTRL_DOWN_MASK); //$NON-NLS-1$
+		menu.addItem("Menu.SAVEAS"); //$NON-NLS-1$
+		menu.addSeparator();
+		menu.addItem("Menu.MERGE",KeyEvent.VK_M,InputEvent.CTRL_DOWN_MASK); //$NON-NLS-1$
+		menu.addSeparator();
+		menu.addItem("Menu.EXIT",KeyEvent.VK_F4,InputEvent.ALT_DOWN_MASK); //$NON-NLS-1$
+
+		mb.add(menu = new GmMenu(Messages.getString("Menu.HELP"),this)); //$NON-NLS-1$
+		menu.addItem("Menu.MANUAL",KeyEvent.VK_F1,0); //$NON-NLS-1$
+		menu.addSeparator();
+		menu.addItem("Menu.ABOUT"); //$NON-NLS-1$
+
+		return mb;
+		}
+
+	protected void addMenuItem(JMenu mb, String key)
+		{
+		JMenuItem mi = new JMenuItem(key);
+		mb.add(mi);
+		mi.setActionCommand(key);
+		mi.addActionListener(this);
+		}
+
+	protected JToolBar createTool()
+		{
+		JToolBar tb = new JToolBar();
+		tb.setFloatable(false);
+
+		addToolItem(tb,"Tool.NEW"); //$NON-NLS-1$
+		addToolItem(tb,"Tool.OPEN"); //$NON-NLS-1$
+		addToolItem(tb,"Tool.SAVE"); //$NON-NLS-1$
+		addToolItem(tb,"Tool.SAVEAS"); //$NON-NLS-1$
+		tb.addSeparator();
+		addToolItem(tb,"Tool.MANUAL"); //$NON-NLS-1$
+
+		return tb;
+		}
+
+	protected void addToolItem(JToolBar tb, String key)
+		{
+		ImageIcon icon = Messages.getIconForKey(key);
+		String tip = Messages.getString(key);
+		JButton b = new JButton(icon);
+		if (icon == null) b.setText(key);
+		b.setToolTipText(tip);
+		b.setActionCommand(key);
+		b.addActionListener(this);
+		tb.add(b);
+		}
+
+	public void save(boolean as)
+		{
+		boolean b;
+		if (as || currentLib.sourceFile == null)
+			b = fc.saveNewFile(currentLib);
+		else
+			b = fc.save(currentLib,currentLib.sourceFile,currentLib.format);
+		if (b) setModified(false);
+		}
+
 	public void setLibrary(Library lib)
 		{
 		currentLib = lib;
 		modified = false;
+		ui.setLibrary(lib);
 		setTitleFile(lib.sourceFile);
+		}
+
+	public void setModified(boolean mod)
+		{
+		if (mod == modified) return;
+		modified = mod;
+		setModifiedTitleIndicator(mod);
+		}
+
+	public boolean saveConfirm()
+		{
+		if (modified)
+			{
+			int r = JOptionPane.showConfirmDialog(this,Messages.getString("LibMaker.SAVE_CHANGES")); //$NON-NLS-1$
+			if (r == JOptionPane.CANCEL_OPTION) return false;
+			if (r == JOptionPane.YES_OPTION) save(false);
+			}
+		return true;
+		}
+
+	/**
+	 * Set whether to display an indicator in the title
+	 * that indicates whether the file has been modified or not.
+	 * Currently the indicator is implemented as an appended asterisk (*).
+	 * <p>
+	 * Setting the indicator to the same value twice has no effect.
+	 * <p>
+	 * Note that any method that sets the title (like setTitleFile)
+	 * will clear the indicator. This is usually convenient since
+	 * you usually only set the title when a file is created or loaded,
+	 * in which case the file will already be unmodified.
+	 * @param mod
+	 */
+	public void setModifiedTitleIndicator(boolean mod)
+		{
+		final String ind = "*";
+		String title = getTitle();
+		if (!mod ^ title.endsWith(ind)) return;
+		setTitle(mod ? title + ind : title.substring(0,title.length() - 1));
 		}
 
 	public void setTitleFile(File f)
 		{
 		String name = f == null ? Messages.getString("LibMaker.NEWLIB") : f.getName(); //$NON-NLS-1$
 		setTitle(Messages.format("LibMaker.TITLE",name)); //$NON-NLS-1$
+		}
+
+	@Override
+	public void actionPerformed(ActionEvent e)
+		{
+		String cmd = e.getActionCommand();
+		if (cmd.endsWith(".NEW")) //$NON-NLS-1$
+			{
+			if (!saveConfirm()) return;
+			setLibrary(new Library());
+			return;
+			}
+		if (cmd.endsWith(".LOAD")) //$NON-NLS-1$
+			{
+			if (!saveConfirm()) return;
+			Library lib = fc.openNewFile();
+			if (lib != null) setLibrary(lib);
+			return;
+			}
+		if (cmd.endsWith(".SAVE")) //$NON-NLS-1$
+			{
+			save(false);
+			return;
+			}
+		if (cmd.endsWith(".SAVEAS")) //$NON-NLS-1$
+			{
+			save(true);
+			return;
+			}
+		if (cmd.endsWith(".EXIT")) //$NON-NLS-1$
+			{
+			if (!saveConfirm()) return;
+			System.exit(0);
+			return;
+			}
+
+		// TODO Respond to moar menu/toolbar actions
+
 		}
 
 	public static void main(String[] args)
---
+++ org/lateralgm/libmaker/mockui/GeneralPane.java
@@ -0,0 +1,205 @@
+/*
+ * Copyright (C) 2012 IsmAvatar <IsmAvatar@gmail.com>
+ * 
+ * This file is part of LibMaker.
+ * LibMaker is free software and comes with ABSOLUTELY NO WARRANTY.
+ * See LICENSE for details.
+ */
+
+package org.lateralgm.libmaker.mockui;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import org.lateralgm.libmaker.Messages;
+import org.lateralgm.libmaker.backend.Action;
+import org.lateralgm.libmaker.backend.Action.Execution;
+import org.lateralgm.libmaker.backend.Action.Kind;
+import org.lateralgm.libmaker.components.EnumRenderer;
+import org.lateralgm.libmaker.components.NumberField;
+import org.lateralgm.libmaker.mockui.MockUI.ActionPanel;
+import org.lateralgm.libmaker.mockui.MockUI.GroupPanel;
+
+public class GeneralPane extends GroupPanel implements ActionPanel,ActionListener
+	{
+	private static final long serialVersionUID = 1L;
+
+	NumberField tActId;
+	JTextField tName, tDesc, tList, tHint, tFunction;
+	JComboBox dKind, dExec;
+	JCheckBox cbHidden, cbAdvanced, cbRegistered;
+	JButton bImageChange, bExecCode;
+	JLabel lExec, lFunc;
+
+	protected void initKeyComponents()
+		{
+		tName = new JTextField();
+		tActId = new NumberField(1,999);
+		tDesc = new JTextField();
+		tList = new JTextField();
+		tHint = new JTextField();
+		tFunction = new JTextField();
+
+		bImageChange = new JButton(Messages.getString("GeneralPane.IMAGE_CHANGE")); //$NON-NLS-1$
+		bImageChange.addActionListener(this);
+
+		String key = "GeneralPane.EXEC_CODE"; //$NON-NLS-1$
+		bExecCode = new JButton(Messages.getString(key),Messages.getIconForKey(key));
+		bExecCode.addActionListener(this);
+
+		dKind = new JComboBox(Action.Kind.values());
+		dKind.setRenderer(new EnumRenderer("ActionKind.")); //$NON-NLS-1$
+		dKind.addActionListener(this);
+
+		dExec = new JComboBox(Action.Execution.values());
+		dExec.setRenderer(new EnumRenderer("Execution.")); //$NON-NLS-1$
+		dExec.addActionListener(this);
+
+		cbHidden = new JCheckBox(Messages.getString("GeneralPane.HIDDEN")); //$NON-NLS-1$
+		cbAdvanced = new JCheckBox(Messages.getString("GeneralPane.ADVANCED")); //$NON-NLS-1$
+		cbRegistered = new JCheckBox(Messages.getString("GeneralPane.PRO")); //$NON-NLS-1$
+
+		//These are key components so we can call .setVisible on them.
+		lExec = new JLabel(Messages.getString("GeneralPane.EXECUTION")); //$NON-NLS-1$
+		lFunc = new JLabel(Messages.getString("GeneralPane.FUNCTION")); //$NON-NLS-1$
+		}
+
+	@Override
+	protected void layoutComponents(GroupLayout layout)
+		{
+		initKeyComponents();
+
+		JLabel lName = new JLabel(Messages.getString("GeneralPane.NAME")); //$NON-NLS-1$
+		JLabel lActId = new JLabel(Messages.getString("GeneralPane.ACT_ID")); //$NON-NLS-1$
+		JLabel lImage = new JLabel(Messages.getString("GeneralPane.IMAGE")); //$NON-NLS-1$
+		JLabel lKind = new JLabel(Messages.getString("GeneralPane.KIND")); //$NON-NLS-1$
+		JLabel lDesc = new JLabel(Messages.getString("GeneralPane.DESCRIPTION")); //$NON-NLS-1$
+		JLabel lList = new JLabel(Messages.getString("GeneralPane.LIST")); //$NON-NLS-1$
+		JLabel lHint = new JLabel(Messages.getString("GeneralPane.HINT")); //$NON-NLS-1$
+
+		JPanel imagePreview = new JPanel();
+		imagePreview.setBorder(BorderFactory.createLoweredBevelBorder());
+		imagePreview.setPreferredSize(new Dimension(32,32));
+
+		int PREF = GroupLayout.PREFERRED_SIZE, DEF = GroupLayout.DEFAULT_SIZE;
+
+		layout.setHorizontalGroup(layout.createSequentialGroup()
+		/**/.addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+		/*	*/.addComponent(lName)
+		/*	*/.addComponent(lActId)
+		/*	*/.addComponent(lImage)
+		/*	*/.addComponent(lKind)
+		/*	*/.addComponent(lExec))
+		/**/.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING,false)
+		/*	*/.addComponent(tName,GroupLayout.Alignment.TRAILING)
+		/*	*/.addComponent(tActId,GroupLayout.Alignment.TRAILING)
+		/*	*/.addGroup(layout.createSequentialGroup()
+		/*		*/.addComponent(imagePreview,PREF,DEF,PREF)
+		/*		*/.addComponent(bImageChange,DEF,93,Short.MAX_VALUE))
+		/*	*/.addComponent(dKind,0,DEF,Short.MAX_VALUE)
+		/*	*/.addComponent(dExec,0,DEF,Short.MAX_VALUE))
+		/*	*/.addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+		/*		*/.addComponent(lDesc)
+		/*		*/.addComponent(lList)
+		/*		*/.addComponent(lHint)
+		/*		*/.addComponent(lFunc))
+		/*	*/.addGroup(layout.createParallelGroup()
+		/*		*/.addComponent(tDesc)
+		/*		*/.addComponent(tList)
+		/*		*/.addComponent(tHint)
+		/*		*/.addGroup(layout.createSequentialGroup()
+		/*			*/.addComponent(cbHidden)
+		/*			*/.addComponent(cbAdvanced)
+		/*			*/.addComponent(cbRegistered))
+		/*		*/.addComponent(tFunction)
+		/*		*/.addComponent(bExecCode)));
+		layout.setVerticalGroup(layout.createSequentialGroup()
+		/**/.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+		/*	*/.addComponent(lName)
+		/*	*/.addComponent(tName,PREF,DEF,PREF)
+		/*	*/.addComponent(lDesc)
+		/*	*/.addComponent(tDesc,PREF,DEF,PREF))
+		/**/.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+		/*	*/.addComponent(lActId)
+		/*	*/.addComponent(tActId,PREF,DEF,PREF)
+		/*	*/.addComponent(lList)
+		/*	*/.addComponent(tList,PREF,DEF,PREF))
+		/**/.addGroup(layout.createParallelGroup(/*not baseline*/)
+		/*	*/.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+		/*		*/.addComponent(lImage)
+		/*		*/.addComponent(bImageChange)
+		/*		*/.addComponent(lHint)
+		/*		*/.addComponent(tHint,PREF,DEF,PREF))
+		/*	*/.addComponent(imagePreview,PREF,DEF,PREF))
+		/**/.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+		/*	*/.addComponent(lKind)
+		/*	*/.addComponent(dKind,PREF,DEF,PREF)
+		/*	*/.addComponent(cbHidden)
+		/*	*/.addComponent(cbAdvanced)
+		/*	*/.addComponent(cbRegistered))
+		/**/.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+		/*	*/.addComponent(lExec)
+		/*	*/.addComponent(dExec,PREF,DEF,PREF)
+		/*	*/.addComponent(lFunc)
+		/*	*/.addComponent(tFunction)
+		/*	*/.addComponent(bExecCode)));
+		}
+
+	@Override
+	public void setComponents(Action a)
+		{
+		tName.setText(a.name);
+		tActId.setText(Integer.toString(a.id));
+		tDesc.setText(a.description);
+		tList.setText(a.list);
+		tHint.setText(a.hint);
+
+		dKind.setSelectedItem(a.kind);
+
+		cbHidden.setSelected(a.hidden);
+		cbAdvanced.setSelected(a.advanced);
+		cbRegistered.setSelected(a.registered);
+
+		dExec.setSelectedItem(a.execType);
+		if (a.execType == Execution.FUNCTION) tFunction.setText(a.execInfo);
+		}
+
+	@Override
+	public void actionPerformed(ActionEvent e)
+		{
+		Object src = e.getSource();
+		if (src == dKind)
+			{
+			boolean normal = dKind.getSelectedItem() == Kind.NORMAL;
+			if (normal ^ dExec.isVisible())
+				{
+				lExec.setVisible(normal);
+				dExec.setVisible(normal);
+				tFunction.setVisible(normal && dExec.getSelectedItem() == Execution.FUNCTION);
+				lFunc.setVisible(tFunction.isVisible());
+				bExecCode.setVisible(normal && dExec.getSelectedItem() == Execution.CODE);
+				}
+			return;
+			}
+		if (src == dExec)
+			{
+			boolean normal = dKind.getSelectedItem() == Kind.NORMAL;
+			tFunction.setVisible(normal && dExec.getSelectedItem() == Execution.FUNCTION);
+			lFunc.setVisible(tFunction.isVisible());
+			bExecCode.setVisible(normal && dExec.getSelectedItem() == Execution.CODE);
+			return;
+			}
+
+		//TODO: bImageChange and bExecCode
+		}
+	}
